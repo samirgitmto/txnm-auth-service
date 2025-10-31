7a1eaa3f8ec2199afd620b5c611c33332809ec9f
samitgitmto <samir.mto@gmail.com>
2025-11-01 00:05:01 +0530

    1st commit

---
+++ src/main/java/com/txnm/auth/TxnmAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.txnm.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnmAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxnmAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/txnm/auth/TxnmAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.txnm.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxnmAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
